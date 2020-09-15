ad973eb199855696a4858fc2d58705c31a3bd43a
Eric <erictes96@gmail.com>
2020-09-16 01:54:10 +1000

    Modify service

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -3,18 +3,13 @@ import { EventService } from './events.service';
 
 @Controller('event')
 export class EventController{
-    constructor(){
+    constructor(private eventService: EventService){
         
     }
 
 @Post()
 sendMessage(@Body('message') msg: string){
-    var TelegrafBot = require('Telegraf');
-    var token= '1398391810:AAHZ4-m9cldolSa_OY-QQ9Vtg2drsQ_i2Yo';
-    var bot = new TelegrafBot(token, );
-    
-      bot.telegram.sendMessage("@testblahblahblah1", msg)
-
+    this.eventService.sendMessage(msg);
     return null;
 }
     
--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -2,5 +2,11 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class EventService{
-
+    sendMessage(msg:string){
+        var TelegrafBot = require('Telegraf');
+        var token= '1398391810:AAHZ4-m9cldolSa_OY-QQ9Vtg2drsQ_i2Yo';
+        var bot = new TelegrafBot(token, );
+    
+        bot.telegram.sendMessage("@testblahblahblah1", msg)
+    }
 }
